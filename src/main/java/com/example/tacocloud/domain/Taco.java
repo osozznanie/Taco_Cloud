77e9c4433e3ae564992dc84aac90b090eb229576
vladcertorygin <vlad.chertorygin18@gmail.com>
2023-10-30 11:22:42 +0200

    added limitation for fields

--- src/main/java/com/example/tacocloud/domain/Taco.java
+++ src/main/java/com/example/tacocloud/domain/Taco.java
@@ -2,11 +2,18 @@ package com.example.tacocloud.domain;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
 public class Taco {
+    @NotNull
+    @Size(min=3, message="Name must be at least 3 characters long")
     private String name;
+
+    @NotNull
+    @Size(min=1, message="You must choose at least 1 ingredient")
     private List<Ingredient> ingredients;
 
 
--- src/main/java/com/example/tacocloud/domain/TacoOrder.java
+++ src/main/java/com/example/tacocloud/domain/TacoOrder.java
@@ -1,19 +1,31 @@
 package com.example.tacocloud.domain;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.CreditCardNumber;
 
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class TacoOrder {
+    @NotBlank(message="Delivery name is required")
     private String deliveryName;
+    @NotBlank(message="Street is required")
     private String deliveryStreet;
+    @NotBlank(message="City is required")
     private String deliveryCity;
+    @NotBlank(message="State is required")
     private String deliveryState;
+    @NotBlank(message="Zip code is required")
     private String deliveryZip;
+    @CreditCardNumber(message="Invalid credit card number")
     private String ccNumber;
     private String ccExpiration;
+    @Size(min=3, max=3, message="CVV must be exactly 3 digits")
     private String ccCVV;
     private List<Taco> tacos = new ArrayList<>();
 
