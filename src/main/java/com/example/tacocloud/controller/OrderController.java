dcfeaa820798648ff1b396549cd951cbb853e740
vladcertorygin <vlad.chertorygin18@gmail.com>
2023-11-09 17:48:49 +0200

    use orderRepo

--- src/main/java/com/example/tacocloud/controller/OrderController.java
+++ src/main/java/com/example/tacocloud/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.example.tacocloud.controller;
 
 import com.example.tacocloud.domain.TacoOrder;
+import com.example.tacocloud.repository.OrderRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
@@ -18,6 +19,14 @@ import javax.validation.Valid;
 @SessionAttributes("tacoOrder")
 public class OrderController {
 
+    public OrderRepository orderRepo;
+
+    public OrderController(OrderRepository orderRepo) {
+        this.orderRepo = orderRepo;
+    }
+
+
+
     @GetMapping("/current")
     public String orderForm() {
         return "orderForm";
@@ -31,8 +40,12 @@ public class OrderController {
             return "orderForm";
         }
         log.info("Order submitted: {}", order);
+        orderRepo.save(order);
         sessionStatus.setComplete();
+
         return "redirect:/";
     }
+
+
 
 }
