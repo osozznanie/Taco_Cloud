9d175a028ea855882856d3edd3a3e5cb30fd4d88
vladcertorygin <vlad.chertorygin18@gmail.com>
2023-11-09 17:44:01 +0200

    redesigned logic

--- src/main/java/com/example/tacocloud/converter/IngredientByIdConverter.java
+++ src/main/java/com/example/tacocloud/converter/IngredientByIdConverter.java
@@ -1,42 +1,22 @@
 package com.example.tacocloud.converter;
 
 import com.example.tacocloud.domain.Ingredient;
+import com.example.tacocloud.repository.IngredientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Component
 public class IngredientByIdConverter implements Converter<String, Ingredient> {
+    private IngredientRepository ingredientRepo;
 
-    private Map<String, Ingredient> ingredientMap = new HashMap<>();
-
-    public IngredientByIdConverter() {
-        ingredientMap.put("FLTO",
-                new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP));
-        ingredientMap.put("COTO",
-                new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP));
-        ingredientMap.put("GRBF",
-                new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN));
-        ingredientMap.put("CARN",
-                new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN));
-        ingredientMap.put("TMTO",
-                new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES));
-        ingredientMap.put("LETC",
-                new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES));
-        ingredientMap.put("CHED",
-                new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE));
-        ingredientMap.put("JACK",
-                new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE));
-        ingredientMap.put("SLSA",
-                new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE));
-        ingredientMap.put("SRCR",
-                new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE));
+    @Autowired
+    public IngredientByIdConverter(IngredientRepository ingredientRepo) {
+        this.ingredientRepo = ingredientRepo;
     }
 
     @Override
     public Ingredient convert(String id) {
-        return ingredientMap.get(id);
+        return ingredientRepo.findById(id).orElse(null);
     }
 }
